4173f19bcafef4fa29383c90bbdbccdabd083635
noureddinezn <noureddinezouana@gmail.com>
2025-12-04 22:23:54 +0100

    la creation de navigation et href de page home

--- templates/header.php
+++ templates/header.php
@@ -3,5 +3,20 @@ $lienclick = $page;
 ?>
 
 <header class="bg-white shadow-md">
-    
+    <nav class="container mx-auto flex justify-between items-center py-4">
+
+        <h1 class="text-2xl font-bold text-blue-600">DigitalWave</h1>
+
+        <ul class="flex space-x-6">
+
+            <li>
+                <a href="/"
+                   class="<?= $lienclick === '/' ? 'text-blue-700 font-bold underline' : 'text-blue-600' ?>">
+                    Accueil
+                </a>
+            </li>
+
+         
+        </ul>
+    </nav>
 </header>
