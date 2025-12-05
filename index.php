977094127468dfbd407faf6f1e188a372a632646
noureddinezn <noureddinezouana@gmail.com>
2025-12-05 15:10:33 +0100

    add hompage routh title and views path

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 
 
 $page = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-// switch ($page) {
+switch ($page) {
+      $title = "NovaCraft Studio - Home";
+        $thispage = "views/home.php";
+        break;
+
+
+
   
-// }
+}
 
 
 
